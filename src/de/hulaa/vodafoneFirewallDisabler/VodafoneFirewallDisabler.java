e10f94992cf714aad5feaece5e5695808e83b5b0
Marcel Hasenbalg <marcel.hasenbalg@googlemail.com>
2022-05-15 16:09:15 +0200

    unix support

--- src/de/hulaa/vodafoneFirewallDisabler/VodafoneFirewallDisabler.java
+++ src/de/hulaa/vodafoneFirewallDisabler/VodafoneFirewallDisabler.java
@@ -1,6 +1,8 @@
 package de.hulaa.vodafoneFirewallDisabler;
 
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
@@ -12,31 +14,52 @@ import org.openqa.selenium.support.ui.Select;
 
 public class VodafoneFirewallDisabler {
 
+	static WebDriver driver;
+	static int stdDelay = 3000;
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(Utils.getOS());
 
-		if(args.length < 3) {
-			System.out.println("Not enough input parameters --> call like this: java -jar vfd.jar http://192.168.0.1 admin password");
-		}else {
-			try {
-				disableFirewall(args[0], args[1], args[2]);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		if (args.length < 3) {
+			System.out.println(
+					"Not enough input parameters --> call like this: java -jar vfd.jar http://192.168.0.1 admin password");
+		} else {
+			//init
+			driver = init();
+			int MINUTES = 20;
+			
+			//loop
+			Timer timer = new Timer();
+			timer.schedule(new TimerTask() {
+			  @Override
+			  public void run() {
+					try {
+						disableFirewall(args[0], args[1], args[2]);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+			  }
+			}, 5000, 1000*60*MINUTES);//wait 0 ms before doing the action and do it evry 1000ms (1second)
 		}
-		
+	}
 
+	public static ChromeDriver init() {
+		String os = Utils.getOS();
+		String driverFile;
+		if (os.toLowerCase().contains("windows")) {
+			driverFile = "chromedriver.exe";
+		} else {
+			driverFile = "chromedriver";
+		}
+		System.setProperty("webdriver.chrome.driver", ("src/de/hulaa/vodafoneFirewallDisabler/" + driverFile));
+		return new ChromeDriver();
 	}
 
 	public static void disableFirewall(String routerURL, String user, String password) throws InterruptedException {
-		// TODO: move to args
-		String routerlogin = routerURL+"/#/login";
-		System.setProperty("webdriver.chrome.driver", "src/de/hulaa/vodafoneFirewallDisabler/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+
+		String routerlogin = routerURL + "/#/login";
+
 		driver.get(routerlogin);
-		Thread.sleep(3000); // Let the user actually see something!
+		Thread.sleep(stdDelay); // Let the user actually see something!
 
 		WebElement userElem = driver.findElement(By.id("username"));
 		WebElement pwElem = driver.findElement(By.id("password"));
@@ -44,86 +67,63 @@ public class VodafoneFirewallDisabler {
 
 		userElem.sendKeys(user);
 		pwElem.sendKeys(password);
-		Thread.sleep(1000);
+		Thread.sleep(stdDelay);
 		loginButton.click();
 
-		Thread.sleep(1000);
-		// TODO: use wrapper function to get element
-		List<WebElement> terminateSessionButton = driver.findElements(By.id("terminateSessionBtn"));
-		if (terminateSessionButton.size() > 0) {
-			terminateSessionButton.get(0).click();
+		Thread.sleep(stdDelay);
+
+		WebElement terminateSessionButton = getElementById("terminateSessionBtn");
+		if(terminateSessionButton != null) {
+			terminateSessionButton.click();
+			Thread.sleep(stdDelay);
 		}
 
-		Thread.sleep(1000);
-		List<WebElement> changeStdPassword = driver.findElements(By.id("CancelCdpButton"));
-		if (changeStdPassword.size() > 0) {
-			changeStdPassword.get(0).click();
-		}
+		WebElement changeStdPassword = getElementById("CancelCdpButton");
+		changeStdPassword.click();
+		Thread.sleep(2*stdDelay);
 
-		Thread.sleep(5000);
-		// TODO: use base url and only refs to #login / #internet
-		driver.get(routerURL+"/#/internet");
-		Thread.sleep(5000);
-		List<WebElement> firewallContainer = driver.findElements(By.id("page_firewall_content_container"));
-		if (firewallContainer.size() > 0) {
-			System.out.println("found firewallContainer...");
+		driver.get(routerURL + "/#/internet");
+		Thread.sleep(2*stdDelay);
 
-			List<WebElement> firewallSwitch = firewallContainer.get(0).findElements(By.tagName("label"));
-			if (firewallSwitch.size() > 0) {
-				System.out.println("found firewallSwitch...");
-
-				boolean firewallActive = firewallContainer.get(0).findElements(By.id("page_firewall_ipv4_switch")).get(0).isSelected();
-				
-				if(firewallActive) {
-					System.out.println("Firewall is active.");
-				}else {
-					System.out.println("Firewall is already disabled.");
-				}
-				
-				
-				if(firewallActive) {
-					// second label element
-					firewallSwitch.get(1).click();
-					Thread.sleep(1000);
-				}
-				//TODO: check if switch is disabled, try again if not
-			}
-
-			List<WebElement> applyButton = driver.findElements(By.id("save_btn"));
-			if (applyButton.size() > 0) {
-				applyButton.get(0).click();
-			}
-			
-			Thread.sleep(1000);
-			List<WebElement> acceptWarningButton = driver.findElements(By.id("applyWarningButton"));
-			if(acceptWarningButton.size() > 0) {
-				acceptWarningButton.get(0).click();
-			}
-
-			
-
-		}
-
-		Thread.sleep(10000); // Let the user actually see something!
+		WebElement firewallContainer = getElementById("page_firewall_content_container");
+		List<WebElement> firewallSwitch = firewallContainer.findElements(By.tagName("label"));
 		
-		//logout
-		driver.get(routerURL+"/#/logout");
-		Thread.sleep(5000);
-		driver.quit();
+		boolean firewallActive;
+		if (firewallSwitch.size() > 0) {
+			System.out.println("found firewallSwitch...");
 
+			firewallActive = firewallContainer.findElements(By.id("page_firewall_ipv4_switch")).get(0).isSelected();
+
+			if (firewallActive) {
+				System.out.println("Firewall is active.");
+				// second label element
+				firewallSwitch.get(1).click();
+				Thread.sleep(stdDelay);
+				System.out.println("Disabled the firewall switch...");
+				
+				WebElement applyButton = getElementById("save_btn");
+				applyButton.click();
+				Thread.sleep(stdDelay);
+
+				WebElement acceptWarningButton = getElementById("applyWarningButton");
+				acceptWarningButton.click();
+				Thread.sleep(4*stdDelay);
+			} else {
+				System.out.println("Firewall is already disabled.");
+			}
+		}
+
+		// logout
+		driver.get(routerURL + "/#/logout");
 	}
 
-	public static void testGoogleSearch() throws InterruptedException {
-		// TODO: change to Resource folder --> make OS dynamix
-		System.setProperty("webdriver.chrome.driver", "src/de/hulaa/vodafoneFirewallDisabler/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.get("http://www.google.com/");
-		Thread.sleep(5000); // Let the user actually see something!
-		WebElement searchBox = driver.findElement(By.name("q"));
-		searchBox.sendKeys("ChromeDriver");
-		searchBox.submit();
-		Thread.sleep(5000); // Let the user actually see something!
-		driver.quit();
+	public static WebElement getElementById(String id) {
+		List<WebElement> elements = driver.findElements(By.id(id));
+		if (elements.size() > 0) {
+			return elements.get(0);
+		} else {
+			return null;
+		}
 	}
 
 }
